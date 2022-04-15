1ba019260845b61a4363b94089096eababca46cf
Mohammed Amir Khan <43793957+mohammedamirk@users.noreply.github.com>
2022-04-15 16:54:08 +0530

    Update aks-cluster.tf

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -14,7 +14,7 @@ resource "azurerm_resource_group" "default" {
 }
 
 resource "azurerm_kubernetes_cluster" "default" {
-  name                = "${random_pet.prefix.id}-aks"
+  name                = var.name
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
   dns_prefix          = "${random_pet.prefix.id}-k8s"
